418122c9b6ab9571f930b2bc56f1233627fdec6d
TK <leandrotk100@gmail.com>
2021-12-21 13:53:14 -0300

    style select

--- findhotel-frontend-assignment/src/components/GuestRooms/GuestRoom/index.tsx
+++ findhotel-frontend-assignment/src/components/GuestRooms/GuestRoom/index.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useContext } from 'react';
+import { css } from '@emotion/css';
 import { AdultsCountInput } from '../AdultsCountInput';
 import { ChildrenCountInput } from '../ChildrenCountInput';
 import { GuestRoomsContext } from '../../../GuestRooms/contexts/GuestRoomsContext';
@@ -23,8 +24,41 @@ export const GuestRoom = ({ room = 'Room 1' }) => {
       <ChildrenCountInput room={room} />
 
       {chidren.map((child, index) => (
-        <div key={`${room}-child-${index}`}>
-          <select onChange={childAgeOnChange(index)} value={child.age}>
+        <div
+          key={`${room}-child-${index}`}
+          className={css`
+            width: 100px;
+          `}
+        >
+          <select
+            onChange={childAgeOnChange(index)}
+            value={child.age}
+            className={css`
+              width: 100%;
+              display: inline-flex;
+              background: rgb(255, 255, 255);
+              padding: 12px 40px 12px 16px;
+              border: 1px solid rgb(190, 210, 233);
+              border-radius: 4px;
+              font-weight: 600;
+              color: rgb(0, 0, 0);
+
+              -webkit-appearance: none;
+              -moz-appearance: none;
+              appearance: none;
+
+              background-image: linear-gradient(
+                  45deg,
+                  transparent 50%,
+                  rgb(0, 113, 243) 50%
+                ),
+                linear-gradient(135deg, rgb(0, 113, 243) 50%, transparent 50%);
+              background-position: calc(100% - 20px) calc(1em + 4px),
+                calc(100% - 15px) calc(1em + 4px), calc(100% - 2.5em) 0.5em;
+              background-size: 5px 5px, 5px 5px, 1px 1.5em;
+              background-repeat: no-repeat;
+            `}
+          >
             {ageOptions.map((_, age) => (
               <option
                 value={age}
