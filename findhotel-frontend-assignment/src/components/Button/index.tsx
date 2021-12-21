edc54022ad9ba52389dd900db7a519df5ca61492
TK <leandrotk100@gmail.com>
2021-12-21 12:21:15 -0300

    better close button

--- findhotel-frontend-assignment/src/components/Button/index.tsx
+++ findhotel-frontend-assignment/src/components/Button/index.tsx
@@ -7,6 +7,7 @@ type ButtonPropTypes = {
   disabled?: boolean;
   onClick: () => void;
   variant?: ButtonVariants;
+  className?: string;
 };
 
 const Cursor = {
@@ -49,37 +50,37 @@ export const Button: FC<ButtonPropTypes> = ({
   disabled = false,
   onClick,
   variant = 'primary',
-}) => {
-  console.log('variant', variant);
-  return (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      className={css`
-        display: inline-flex;
-        border: 0px;
-        border-radius: 6px;
-        margin: 0px;
-        cursor: ${Cursor[variant]};
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        vertical-align: middle;
-        position: relative;
-        text-decoration: none;
-        font-size: 16px;
-        font-weight: 600;
-        padding: ${variant === 'danger' ? '8px' : '16px 32px'};
-        color: ${Colors[variant]};
-        background-color: ${BackgroundColors[variant]};
-        box-shadow: ${BoxShadow[variant]};
+  className,
+}) => (
+  <button
+    disabled={disabled}
+    onClick={onClick}
+    className={css`
+      display: inline-flex;
+      border: 0px;
+      border-radius: 6px;
+      margin: 0px;
+      cursor: ${Cursor[variant]};
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      vertical-align: middle;
+      position: relative;
+      text-decoration: none;
+      font-size: 16px;
+      font-weight: 600;
+      padding: 16px 32px;
+      color: ${Colors[variant]};
+      background-color: ${BackgroundColors[variant]};
+      box-shadow: ${BoxShadow[variant]};
 
-        &:hover {
-          background-color: ${BackgroundColorsHover[variant]};
-        }
-      `}
-    >
-      {children}
-    </button>
-  );
-};
+      &:hover {
+        background-color: ${BackgroundColorsHover[variant]};
+      }
+
+      ${className}
+    `}
+  >
+    {children}
+  </button>
+);
--- findhotel-frontend-assignment/src/components/GuestRooms/CloseButton/index.tsx
+++ findhotel-frontend-assignment/src/components/GuestRooms/CloseButton/index.tsx
@@ -1,4 +1,5 @@
 import { FC, useContext } from 'react';
+import { css } from '@emotion/css';
 import { GuestRoomsContext } from '../../../GuestRooms/contexts/GuestRoomsContext';
 import { Button } from '../../Button';
 import { ReactComponent as CloseIcon } from '../../Button/close.svg';
@@ -16,7 +17,13 @@ export const CloseButton: FC<CloseButtonPropTypes> = ({ room, index }) => {
   };
 
   return (
-    <Button variant="danger" onClick={removeOnClick(index)}>
+    <Button
+      variant="danger"
+      onClick={removeOnClick(index)}
+      className={css`
+        padding: 8px;
+      `}
+    >
       <CloseIcon />
     </Button>
   );
