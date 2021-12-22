df7a54bc7b90dcde2226e993f5fc68a8b87a0f84
TK <leandrotk100@gmail.com>
2021-12-22 14:28:29 -0300

    extract css

--- findhotel-frontend-assignment/src/components/GuestRoomOverlay/index.tsx
+++ findhotel-frontend-assignment/src/components/GuestRoomOverlay/index.tsx
@@ -6,10 +6,22 @@ import { SearchButton } from '../GuestRooms/SearchButton';
 import { Header } from './Header';
 
 const overlayClassName = css`
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-  // height: 100%;
+  inset: 0px;
+  position: fixed;
+  z-index: 111;
+`;
+
+const blackdropClassName = css`
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  position: fixed;
+  background: rgba(42, 51, 61, 0.6);
+  opacity: 1;
+  will-change: opacity;
+  transition: opacity 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 `;
 
 const modelClassName = css`
@@ -18,7 +30,7 @@ const modelClassName = css`
   position: relative;
   height: unset;
   width: 400px;
-  left: 35%;
+  left: 40%;
   top: 15%;
   max-height: 65%;
   border-radius: 6px;
@@ -39,36 +51,14 @@ export const GuestRoomOverlay: FC<GuestRoomOverlayPropTypes> = ({
   open = false,
 }) =>
   open ? (
-    <div
-      className={css`
-        inset: 0px;
-        position: fixed;
-        z-index: 111;
-      `}
-    >
-      <div
-        onClick={onClose}
-        className={css`
-          top: 0px;
-          left: 0px;
-          width: 100%;
-          height: 100%;
-          z-index: -1;
-          position: fixed;
-          background: rgba(42, 51, 61, 0.6);
-          opacity: 1;
-          will-change: opacity;
-          transition: opacity 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-        `}
-      />
+    <div className={overlayClassName}>
+      <div onClick={onClose} className={blackdropClassName} />
       <GuestRoomsProvider guestRoomsString={guestRoomsString}>
-        {/* <div className={overlayClassName}> */}
         <div className={modelClassName}>
           <Header onClose={onClose} />
           <GuestRooms />
           <SearchButton />
         </div>
-        {/* </div> */}
       </GuestRoomsProvider>
     </div>
   ) : null;
