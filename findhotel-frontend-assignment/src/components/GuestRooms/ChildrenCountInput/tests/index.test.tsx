5f0219373bfc6aef2fdd47e1d94a4416738933b8
TK <leandrotk100@gmail.com>
2021-12-23 18:27:26 -0300

    add test for children count input

---
+++ findhotel-frontend-assignment/src/components/GuestRooms/ChildrenCountInput/tests/index.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ChildrenCountInput } from '..';
+import { GuestRoomsProvider } from '../../../../GuestRooms/contexts/GuestRoomsContext';
+
+describe('ChildrenCountInput', () => {
+  it('renders the button', () => {
+    render(
+      <GuestRoomsProvider>
+        <ChildrenCountInput roomIndex={0} />
+      </GuestRoomsProvider>
+    );
+
+    const count = screen.getByText('0');
+    expect(count).toBeInTheDocument();
+
+    const plusButton = screen.getAllByRole('button')[1];
+    userEvent.click(plusButton);
+    userEvent.click(plusButton);
+    const increasedCount = screen.getByText('2');
+
+    expect(increasedCount).toBeInTheDocument();
+    const minusButton = screen.getAllByRole('button')[0];
+    userEvent.click(minusButton);
+    const decreasedCount = screen.getByText('1');
+    expect(decreasedCount).toBeInTheDocument();
+  });
+});
