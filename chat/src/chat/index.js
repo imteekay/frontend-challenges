a616928e8fdd84f6084070419c0bd3f9663719b3
TK <leandrotk100@gmail.com>
2022-07-27 23:04:49 -0300

    PoC: chat working

--- chat/src/chat/index.js
+++ chat/src/chat/index.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Socket from 'socket.io-client';
+
+const SERVER_URL = 'localhost:5000';
 
 export const Chat = () => {
-  return <h1>Chat</h1>;
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const socket = Socket(SERVER_URL);
+
+  socket.on('message', (msg) => {
+    setMessages([...messages, msg]);
+  });
+
+  return (
+    <>
+      <h1>Chat</h1>
+      <input onChange={(event) => setMessage(event.target.value)} />
+      <button onClick={() => socket.emit('message', message)}>Send</button>
+
+      <hr></hr>
+
+      <div>
+        {messages.map((msg) => (
+          <p>{msg}</p>
+        ))}
+      </div>
+    </>
+  );
 };
